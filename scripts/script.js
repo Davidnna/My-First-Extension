1b79395c35ad2e70bcf6dc51bd8addcac39590cd
Davidnna <davedozie1@gmail.com>
2025-02-27 01:01:56 +0100

    Correct Arrangement

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,18 @@
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+let myLeads = []
 
+// Retrieve Links
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
-
+// Display Links
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
@@ -33,15 +27,28 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+// Save Link
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value.trim === "") {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    }
+})
+
+// Save Tab Current Link
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+// Delete Links
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    render(myLeads)
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
